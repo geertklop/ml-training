b01c3990ed3ac6ff3da51c4448a536ccbb68b21c
geertklop <geertklop@gmail.com>
2020-05-22 14:08:14 +0200

    Simple cleanup vanilla neural network code

--- vanilla-nn/basic_nn.py
+++ vanilla-nn/basic_nn.py
@@ -1,20 +1,28 @@
-import os
-print(os.getcwd())
-
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from nn.utils import sigmoid, load_planar_dataset
+from utils import sigmoid, load_planar_dataset
 
-# 1 hidden layer network with cross-entropy cost
+
 class NeuralNet:
+    """Implementation of a n hidden layer network with SGD
+
+    Args:
+        dimensions: (tpl/ list) Dimensions of the neural net.
+            (input, hidden layer, output)
+
+    Attributes:
+        n_layers: Number of hidden layers
+        loss: Current loss
+        loss_dict: Loss history (evcery 100th epoch)
+        learning_rate: Learning rate parameter for updating weights
+        w: Weights matrix per layer'
+        b: Bias matrix per layer
+        acitvations: activation function per layer
+        z: linear outcome per layer (current)
+        a: activated outcome per layer (current)
+    """
+
     def __init__(self, dimensions):
-        """
-        Arguments:
-        dimensions: (tpl/ list) Dimensions of the neural net. (input, hidden layer, output)
-        activations: (tpl/ list) Activations functions, if not supplied all are sigmoid.
-        """
         np.random.seed(2)
 
         self.n_layers = len(dimensions)
@@ -35,7 +43,8 @@ class NeuralNet:
         self.z = {}
         self.a = {}
 
-    def forward_propagation(self, X):
+    def _forward_propagation(self, X):
+        """Single forward propagation pass"""
         self.z = {}
         self.a = {0: X}
 
@@ -46,34 +55,40 @@ class NeuralNet:
         return self.z, self.a
 
     def predict(self, X):
-        _, a = self.forward_propagation(X)
+        """Feed forward pass and return output of final layer"""
+        _, a = self._forward_propagation(X)
 
         return a[self.n_layers - 1]
 
-    def compute_cost(self, y_pred, y):
+    def _compute_cost(self, y_pred, y):
         """
         Computes the cross-entropy cost
 
-        Arguments:
-        Y -- "true" labels vector of shape (1, number of examples)
+        Args:
+            y (array): Predicted outcomes
+            y_pred (array): True labels
 
         Returns:
-        cost -- cross-entropy cost given equation (13)
+            cost (float): cross-entropy cost given equation
         """
 
         m = y.shape[1]  # number of training examples
 
         # Compute the cross-entropy cost
-        logprobs = np.multiply(np.log(y_pred), y) + np.multiply((1 - y), np.log(1 - y_pred))
+        logprobs = np.multiply(np.log(y_pred), y) + \
+            np.multiply((1 - y), np.log(1 - y_pred))
+
         cost = - np.sum(logprobs) / m
 
-        self.loss = np.squeeze(cost)  # makes sure cost is the dimension we expect.
-        # E.g., turns [[17]] into 17
+        # Make sure cost is right dimension. E.g., turns [[17]] into 17
+        self.loss = np.squeeze(cost)
+
         assert (isinstance(self.loss, float))
 
         return self.loss
 
-    def update_parameters(self, update_params):
+    def _update_parameters(self, update_params):
+        """Update weights and biases with gradient and learning rate"""
         for k, v in update_params.items():
             dw = v[0]
             db = v[1]
@@ -81,17 +96,14 @@ class NeuralNet:
             self.w[k] -= self.learning_rate * dw
             self.b[k] -= self.learning_rate * db
 
-
-    def backward_propagation(self, X, y):
+    def _backward_propagation(self, X, y):
         """
-        Arguments:
-        parameters -- python dictionary containing our parameters
-        cache -- a dictionary containing "Z1", "A1", "Z2" and "A2".
-        X -- input data of shape (2, number of examples)
-        Y -- "true" labels vector of shape (1, number of examples)
+        Args:
+            X (array): input data of shape (2, number of examples)
+            Y (array): labels vector of shape (1, number of examples)
 
         Returns:
-        grads -- python dictionary containing your gradients with respect to different parameters
+        dict: gradients with respect to different parameters
         """
         m = X.shape[1]
         update_params = {}
@@ -105,34 +117,54 @@ class NeuralNet:
             db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
 
             if i > 1:
-                dz = np.multiply(np.dot(self.w[i].T, dz), 1 - np.power(self.a[i-1], 2))
+                dz = np.multiply(np.dot(self.w[i].T, dz),
+                                 1 - np.power(self.a[i-1], 2))
 
             update_params[i] = (dw, db)
 
         return update_params
 
     def fit(self, X, y, epochs=10000, learning_rate=1.2, print_cost=True):
+        """Fit model to input data.
+
+        Implemented is a full training loop of:
+        - Forward propagation
+        - Cost calculation
+        - Backpropagation
+        - Update weights and biases using Stochastic Gradient Descent
+
+        Args:
+            X (array): Features vector
+            y (array): Label vectors
+            epochs (int, optional): Number of training rounds.
+                Defaults to 10000.
+            learning_rate (float, optional): Learning rate to use for
+                gradient descent. Defaults to 1.2.
+            print_cost (bool, optional): Whether to print the cost each
+                1000th epoch. Defaults to True.
+        """
         self.learning_rate = learning_rate
         # Loop (gradient descent)
         for i in range(0, epochs):
-            # Forward propagation. Inputs: "X, parameters". Outputs: "A2, cache".
+            # Forward propagation
             y_pred = self.predict(X)
 
-            # Cost function. Inputs: "A2, Y, parameters". Outputs: "cost".
-            cost = self.compute_cost(y_pred, y)
+            # Calculate loss
+            cost = self._compute_cost(y_pred, y)
 
-            # Backpropagation. Inputs: "parameters, cache, X, Y". Outputs: "grads".
-            grads = self.backward_propagation(X, y)
+            # Backpropagation
+            grads = self._backward_propagation(X, y)
 
-            # Gradient descent parameter update. Inputs: "parameters, grads". Outputs: "parameters".
-            self.update_parameters(grads)
+            # Gradient descent parameter update.
+            self._update_parameters(grads)
 
             # Print the cost every 1000 iterations
             if print_cost and i % 1000 == 0:
                 self.loss_dict[i] = self.loss
                 print("Cost after iteration %i: %f" % (i, cost))
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     X_train, y_train = load_planar_dataset(m=1000)
     X_test, y_test = load_planar_dataset(m=400)
 
@@ -142,15 +174,9 @@ if __name__=='__main__':
 
     nn = NeuralNet(dimensions=(n_x, n_h, n_y))
     nn.fit(X_train, y_train)
-    
+
     preds = np.rint(nn.predict(X_train))
     print("Accuracy Train: {}".format(accuracy_score(y_train[0], preds[0])))
 
     preds = np.rint(nn.predict(X_test))
     print("Accuracy Test: {}".format(accuracy_score(y_test[0], preds[0])))
-
-
-
-
-
-
